8c81076d0de033faba1f55f7afee07db3edf4462
karthick <itkarthi02@gmail.com>
2013-08-15 18:04:47 +0530

    Initial commit

---
+++ karthi_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KarthiApp::Application.config.secret_key_base = '52ca1f711afafbf81efc245127944024a102a09d4481d69921cb2e069c3da649e1229d8e69a5ab2f84b5b5791d66aba159cf1f6526ec7b2c352ccd306b4e0b2c'
